a0d098833223392c033a00bb22fe58c3baa9ef25
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-06-14 23:19:20 -0400

    Implement second stage changes and styles

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import ReactTimeout from 'react-timeout';
 
 class SmurfForm extends Component {
   state = {
     name: '',
     age: '',
-    height: ''
+    height: '',
+    done: false,
+    redirect: false
   };
 
   addSmurf = e => {
@@ -23,8 +26,11 @@ class SmurfForm extends Component {
     this.setState({
       name: '',
       age: '',
-      height: ''
+      height: '',
+      done: true
     });
+
+    this.props.setTimeout(_ => this.setState({ done: false }), 1000);
   }
 
   handleInputChange = e => {
@@ -33,31 +39,36 @@ class SmurfForm extends Component {
 
   render() {
     return (
-      <div className="SmurfForm">
+      <div className="smurf-form">
         <form onSubmit={this.addSmurf}>
           <input
+            autoFocus
+            className="name"
             onChange={this.handleInputChange}
             placeholder="name"
             value={this.state.name}
             name="name"
           />
           <input
+            className="age"
             onChange={this.handleInputChange}
             placeholder="age"
             value={this.state.age}
             name="age"
           />
           <input
+            className="height"
             onChange={this.handleInputChange}
             placeholder="height"
             value={this.state.height}
             name="height"
           />
-          <button type="submit">Add to the village</button>
+          <button type="submit">ADD</button>
         </form>
+        {this.state.done && <p>ADDED SMURF</p>}
       </div>
     );
   }
 }
 
-export default SmurfForm;
+export default ReactTimeout(SmurfForm);
